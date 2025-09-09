caf4827833af9733a7859766664f09379e3e0924
Ludwig Laukka <ludw@kth.se>
2025-09-09 13:57:56 +0200

    plan för cyberklara

---
+++ CyberClara/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+    
+    planen är som följer
+    gör en array med alla förnamnen
+    gör en ny array med alla förnamnen från index i första arrayen + alla efternamnen
+    sorta nya arrayen och ta bort alla som är likadana som sin granne eller remove duplicates eller gå igenom alla och gör ytterligare en ny array med alla fast lägg inte till dem mer än 1 gång
+
+}
